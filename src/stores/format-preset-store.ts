f9ec0398e3d30b8c8ab913935a1b021704cb25a9
Paul Wilkinson <cremoni@gmail.com>
2026-02-27 09:09:38 +0100

    new presets

--- src/stores/format-preset-store.ts
+++ src/stores/format-preset-store.ts
@@ -152,6 +152,89 @@ export const DEVICE_PRESETS: FormatPreset[] = [
     },
   },
   {
+    id: "squid-salmple",
+    name: "Squid Salmple",
+    settings: {
+      ...DEFAULT_FORMAT_SETTINGS,
+      fileFormat: "WAV",
+      sampleDepth: "16-bit",
+      mono: true,
+    },
+  },
+  {
+    id: "morphagene",
+    name: "Morphagene",
+    settings: {
+      ...DEFAULT_FORMAT_SETTINGS,
+      fileFormat: "WAV",
+      sampleDepth: "16-bit",
+      sampleRate: "48000",
+    },
+  },
+  {
+    id: "erica-sampledrum",
+    name: "Erica Synths Sampledrum",
+    settings: {
+      ...DEFAULT_FORMAT_SETTINGS,
+      fileFormat: "WAV",
+      sampleDepth: "16-bit",
+      sampleRate: "44100",
+    },
+  },
+  {
+    id: "tiptop-one",
+    name: "Tiptop One",
+    settings: {
+      ...DEFAULT_FORMAT_SETTINGS,
+      fileFormat: "WAV",
+      sampleDepth: "16-bit",
+      sampleRate: "44100",
+      mono: true,
+    },
+  },
+  {
+    id: "4ms-sts",
+    name: "4MS STS",
+    settings: {
+      ...DEFAULT_FORMAT_SETTINGS,
+      fileFormat: "WAV",
+      sampleDepth: "16-bit",
+      sampleRate: "44100",
+    },
+  },
+  {
+    id: "addac-112",
+    name: "ADDAC 112 Looper & Granular Sampler",
+    settings: {
+      ...DEFAULT_FORMAT_SETTINGS,
+      fileFormat: "WAV",
+      sampleDepth: "16-bit",
+      sampleRate: "44100",
+    },
+  },
+  {
+    id: "addac-wavplayer",
+    name: "ADDAC 101 WAV Player",
+    settings: {
+      ...DEFAULT_FORMAT_SETTINGS,
+      fileFormat: "WAV",
+      sampleDepth: "16-bit",
+      sampleRate: "44100",
+      mono: true,
+    },
+  },
+  {
+    id: "disting-mk4",
+    name: "Disting MK4",
+    settings: {
+      ...DEFAULT_FORMAT_SETTINGS,
+      fileFormat: "WAV",
+      sampleDepth: "16-bit",
+      sampleRate: "44100",
+      mono: true,
+    },
+  },
+  {
     id: "polyend-tracker",
     name: "Polyend Tracker",
     settings: {
@@ -182,7 +265,7 @@ interface FormatPresetStoreState {
 export const useFormatPresetStore = create<FormatPresetStoreState>((set, get) => ({
   currentPreset: {
     id: "current",
-    name: "Current",
+    name: "User",
     settings: DEFAULT_FORMAT_SETTINGS,
   },
   selectedPresetId: "current",
