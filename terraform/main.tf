1d3fbe3243f64a20832356a5015316d795f5048a
Paul Wilkinson <cremoni@gmail.com>
2026-03-09 17:44:10 +0100

    db password for staging

--- terraform/main.tf
+++ terraform/main.tf
@@ -338,7 +338,7 @@ resource "aws_secretsmanager_secret" "database_url" {
 
 resource "aws_secretsmanager_secret_version" "database_url" {
   secret_id     = aws_secretsmanager_secret.database_url.id
-  secret_string = "postgresql://${var.db_username}:${var.db_password}@${aws_db_instance.postgres.endpoint}/octacard"
+  secret_string = "postgresql://${var.db_username}:${urlencode(var.db_password)}@${aws_db_instance.postgres.endpoint}/octacard"
   depends_on   = [aws_db_instance.postgres]
   lifecycle { ignore_changes = [secret_string] }
 }
@@ -629,7 +629,7 @@ output "s3_bucket_arn" {
 }
 
 output "database_url" {
-  value       = "postgresql://${var.db_username}:${var.db_password}@${aws_db_instance.postgres.endpoint}/octacard"
+  value       = "postgresql://${var.db_username}:${urlencode(var.db_password)}@${aws_db_instance.postgres.endpoint}/octacard"
   description = "Postgres connection string (DATABASE_URL)"
   sensitive   = true
 }
